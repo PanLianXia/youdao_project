e8a6e52a1ef3662ded35622792b7f60276ac6d09
DESKTOP-542RING\IT_PanLianxia <plx8624@163.com>
2022-05-16 10:57:52 +0800

    yarn eject

--- React/project/react-demo/src/App.js
+++ React/project/react-demo/src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
+        </a> */}
+      <div class="row header">
+        <div class="col-md-12">头部</div>
+      </div>
+      <div class="row main">
+        <div class="col-md-2 sidbar">侧边栏</div>
+        <div class="col-md-10 content">内容区</div>
+      </div>
+      <div class="row fotter">
+        <div class="col-md-12">底部</div>
+      </div>
+      {/* </header> */}
     </div>
   );
 }
