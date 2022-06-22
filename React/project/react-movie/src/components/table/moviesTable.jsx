13be663cf3a93335c6bb3194a01b65e913356d62
赵帅 <zhaoshuai@ysjhacc.com>
2022-06-22 19:33:14 +0800

    react movies test

---
+++ React/project/react-movie/src/components/table/moviesTable.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Like from '../common/like';
+import TableBody from './tableBody';
+import TableHeader from './tableHeader';
+const MoviesTable = ({ movies, onLike }) => {
+  const columns = [
+    {
+      path: "title",
+      label: "标题",
+      content: (movie) => (
+        <a target="_blank" href={movie.alt}>
+          {movie.title}
+        </a>
+      ),
+    },
+    { path: "original_title", label: "原标题" },
+    { path: "year", label: "上映时间" },
+    { path: "genres", label: "分类" },
+    { path: "rating.average", label: "评分" },
+    {
+      key: "like",
+      label: "收藏",
+      content: (movie) => <Like liked={movie.liked} onClick={() => onLike(movie)} />,
+    },
+  ];
+
+  return ( 
+    <table className='table'>
+      <TableHeader columns={columns} />
+      <TableBody columns={columns} data={movies} />
+    </table>
+   );
+}
+
+export default MoviesTable;
